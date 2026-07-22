b2d21e1f89993bf9de7bf6b64f2e53242d9d66b8
Splleat <splleat@gmail.com>
2026-07-22 22:59:39 +0900

    feat: Reactor 자동 컨텍스트 전파 설정 추가

--- src/main/java/com/nhnacademy/front/FrontApplication.java
+++ src/main/java/com/nhnacademy/front/FrontApplication.java
@@ -2,11 +2,13 @@ package com.nhnacademy.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class FrontApplication {
 
     public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(FrontApplication.class, args);
     }
 
