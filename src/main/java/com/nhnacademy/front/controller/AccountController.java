718151bf3e4c93d3fe451c4e2e48a7e8b9233447
konayeong <dnjfrhkd402@naver.com>
2026-07-28 09:13:38 +0900

    feat : 회원정보 수정, 비밀번호 변경, 탈퇴 layout

--- src/main/java/com/nhnacademy/front/controller/AccountController.java
+++ src/main/java/com/nhnacademy/front/controller/AccountController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccountController {
+    // [임시] 담당자가 직접 url 변경하기
     @GetMapping("/login")
     public String test() {
        return "auth/login";
@@ -20,8 +21,12 @@ public class AccountController {
         return "auth/forgot-password";
     }
 
-    @GetMapping("/forgot-id")
-    public String forgotId() {
-        return "auth/forgot-id";
-    }
+    @GetMapping("/mypage")
+    public String info() { return "account/account_info";}
+
+    @GetMapping("/mypage/change-password")
+    public String password() { return "account/change_password"; }
+
+    @GetMapping("/withdraw")
+    public String withdraw() { return "account/withdraw"; }
 }
