e9b1110a7ef3f3fd13a4318f0b8f05047e74a0a2
Duwing <ssuk0406@gmail.com>
2026-08-13 18:24:07 +0900

    hotfix: 오타 수정

--- src/test/java/com/nhnacademy/front/auth/controller/AuthControllerTest.java
+++ src/test/java/com/nhnacademy/front/auth/controller/AuthControllerTest.java
@@ -23,7 +23,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-가import java.util.List;
+import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.Matchers.containsString;
