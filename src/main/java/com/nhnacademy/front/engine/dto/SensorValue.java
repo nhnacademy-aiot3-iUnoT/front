ccc6f1b3822b2b198012931de1c56a658f4af741
Duwing <ssuk0406@gmail.com>
2026-08-14 10:16:17 +0900

    feat: 임시 수정

--- src/main/java/com/nhnacademy/front/engine/dto/SensorValue.java
+++ src/main/java/com/nhnacademy/front/engine/dto/SensorValue.java
@@ -2,16 +2,22 @@ package com.nhnacademy.front.engine.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-public record SensorValueRange(
+public record SensorValue(
+
+        GenerationMode mode,
 
         @NotNull(message = "최솟값은 필수입니다.")
         Double min,
 
         @NotNull(message = "최댓값은 필수입니다.")
-        Double max
+        Double max,
+
+        Double fixedValue,
+
+        Double probability
 
 ) {
-    public SensorValueRange {
+    public SensorValue {
         if (min != null && max != null && min > max) {
             throw new IllegalArgumentException(
                     "최솟값은 최댓값보다 클 수 없습니다."
--- src/main/java/com/nhnacademy/front/engine/dto/request/VirtualSensorCreateRequest.java
+++ src/main/java/com/nhnacademy/front/engine/dto/request/VirtualSensorCreateRequest.java
@@ -1,13 +1,12 @@
 package com.nhnacademy.front.engine.dto.request;
 
 import com.nhnacademy.front.engine.dto.SensorType;
-import com.nhnacademy.front.engine.dto.SensorValueRange;
+import com.nhnacademy.front.engine.dto.SensorValue;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 
 public record VirtualSensorCreateRequest(
 
@@ -20,8 +19,8 @@ public record VirtualSensorCreateRequest(
 
         @NotEmpty(message = "센서 타입을 하나 이상 선택해야 합니다.")
         Map<@NotNull SensorType,
-              @Valid SensorValueRange
-        > virtualSensorValueRanges,
+              @Valid SensorValue
+        > virtualSensorValues,
 
 
         @DecimalMin(value = "0.0", message = "문 열림 확률은 0 이상이어야 합니다.")
@@ -31,10 +30,10 @@ public record VirtualSensorCreateRequest(
 
     @AssertTrue(message = "선택한 센서 타입의 설정값을 입력해야 합니다.")
     public boolean isSelectedSensorConfigurationValid() {
-        if (virtualSensorValueRanges == null || virtualSensorValueRanges.isEmpty()) {
+        if (virtualSensorValues == null || virtualSensorValues.isEmpty()) {
             return true; // 비어 있는 검증은 @NotEmpty에서 예외 처리
         }
 
-        return virtualSensorValueRanges.values().stream().noneMatch(Objects::isNull);
+        return virtualSensorValues.values().stream().noneMatch(Objects::isNull);
     }
 }
