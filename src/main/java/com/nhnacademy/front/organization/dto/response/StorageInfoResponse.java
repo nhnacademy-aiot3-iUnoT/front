3472ddbf596a01cdc554a9f8a831088c5148a069
{gogooga1} <{dudrn322056@naver.com}>
2026-08-24 15:53:33 +0900

    hotfix : StorageInfoResponse롤백 수정

--- src/main/java/com/nhnacademy/front/organization/dto/response/StorageInfoResponse.java
+++ src/main/java/com/nhnacademy/front/organization/dto/response/StorageInfoResponse.java
@@ -1,19 +1,9 @@
 package com.nhnacademy.front.organization.dto.response;
 
-import com.nhnacademy.front.organization.dto.StorageStatus;
-
-import java.time.LocalDateTime;
 
 public record StorageInfoResponse(
-
         Long storageId,
         Long organizationId,
-        String name,
-        String description,
-        StorageStatus status,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt
-
-
+        String name
 ) {
 }
