92c3f3aecff0a203109c4c43e81cb7374bee09bd
Duwing <ssuk0406@gmail.com>
2026-08-31 13:25:48 +0900

    fix: import가 빠져있던 문제 수정

--- src/main/java/com/nhnacademy/front/global/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/front/global/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.Cookie;
 import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
